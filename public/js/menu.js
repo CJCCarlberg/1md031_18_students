6ce6e2bb5994c33cd657aff23316be43a46966ee
CJCCarlberg <cj@castencarlberg,se>
2018-11-19 15:12:42 +0100

    latest version

---
+++ public/js/menu.js
@@ -0,0 +1,6 @@
+var food = [
+    {"name": "Ostburgare", "taste": "Horrible", "gluten": false, "lactose": true,"picture": "https://supervalu.co.uk/wp-content/uploads/2018/09/Cheeseburger.png", "foodCheck": false,},
+    {"name": "Burger1 ", "taste": "Tastes good ", "gluten": true, "lactose": true, "picture": "https://i2.wp.com/fullofplants.com/wp-content/uploads/2018/01/smoky-tempeh-black-bean-burgers-14.jpg?w=1400&ssl=12", "foodCheck": false,},
+    {"name": "Burger2 ", "taste": "Tastes bad ", "gluten": false, "lactose": true, "picture": "https://www.wearesovegan.com/wp-content/uploads/2018/03/SV_GreenBurger_V1_Header1.jpg", "foodCheck": false,},
+    {"name": "Burger3 ", "taste": "Tastes medium ", "gluten": true, "lactose": true, "picture": "https://mittkok.expressen.se/wp-content/uploads/2017/06/cajunboken-2017-blackeyed-been-burger-700x700.jpg", "foodCheck": false,},
+]
